23a59a9c7a50cf3941b8753a01bb45e3aed7eae4
miguelmilagres <miguelmdemacedo@gmail.com>
2024-03-26 16:45:57 -0300

    exception exercise solution

---
+++ src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+import exceptions.BusinessException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double inicialBalance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account account = new Account(number, holder, inicialBalance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			account.withdraw(amount);
+			System.out.printf("New balance: %.2f", account.getBalance());
+		}
+		catch (BusinessException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		sc.close();;
+	}
+
+}
